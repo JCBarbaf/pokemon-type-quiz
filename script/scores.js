4f98bdf5dce689412209ea8306232a0488f427b9
jcbarbaf <jcbarbaf@gmail.com>
2024-02-15 14:47:52 +0100

    problema best score isnan

--- script/scores.js
+++ script/scores.js
@@ -4,6 +4,10 @@ class Scores extends HTMLElement {
       super()
       this.shadow = this.attachShadow({ mode: 'open' })
       this.score = 0
+      if (isNaN(localStorage.getItem("bestScore"))) {
+        localStorage.setItem("bestScore", 0)
+      }
+      
     }
   
     connectedCallback () {
@@ -64,7 +68,7 @@ class Scores extends HTMLElement {
           <svg class="star" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M50 0L61.2257 34.5491H97.5528L68.1636 55.9017L79.3893 90.4509L50 69.0983L20.6107 90.4509L31.8364 55.9017L2.44717 34.5491H38.7743L50 0Z" fill="#C4C411"/>
           </svg>
-          <p class="best-score">${localStorage.getItem("bestScore") || 0}</p>
+          <p class="best-score">${0 || localStorage.getItem("bestScore")}</p>
         </div>
       </div>
       `
